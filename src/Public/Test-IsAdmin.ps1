65507d1ebbf31ab3a5fc04a149d7c7b57aa5839e
Peter Wawa <meelisn@outlook.com>
2026-05-29 16:07:29 +0300

    Add other platform support to function

--- src/Public/Test-IsAdmin.ps1
+++ src/Public/Test-IsAdmin.ps1
@@ -1,8 +1,14 @@
-function Test-IsAdmin {
+﻿function Test-IsAdmin {
     # .EXTERNALHELP telia.windows.management-help.xml
-    #[OutputType([bool])]
+    [OutputType([bool])]
+    [CmdletBinding()]
+    param ()
 
-    $CurrentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
-    $Role = [Security.Principal.WindowsBuiltinRole]::Administrator
-    ([Security.Principal.WindowsPrincipal] $CurrentUser).IsInRole($Role)
+    if ($PSVersionTable.PSVersion -lt '5.1' -or $PSEdition -eq 'Desktop' -or $IsWindows) {
+        $CurrentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+        $Role = [Security.Principal.WindowsBuiltinRole]::Administrator
+        ([Security.Principal.WindowsPrincipal] $CurrentUser).IsInRole($Role)
+    } else {
+        (id -u) -eq 0
+    }
 }
